2f69c122795dec998390c0bcc705022be53a916d
Josh Kelley <joshkel@gmail.com>
2016-08-14 17:39:30 -0400

    Fix connecting line at right edge
    Insert a null to force a gap, instead of leaving a line at the right edge.

--- jquery.flot.reflect.js
+++ jquery.flot.reflect.js
@@ -5,11 +5,6 @@
  *
  * - Reflects data.  Currently, only reflecting (negating) the Y value is supported.
  * - Reflected portion is appended to current data so Flot won't treat the reflection as another line.
- * - Since it is one line, the reflected portion has to start from the end; otherwise Flot would draw
- *     a line across the data to link the end of the line and the beginning of the reflection.
- *     Note that this line between the original data and its reflection is still drawn on the
- *     right side of the screen, but it is in the least noticeable/obtrusive way until
- *     a better solution can be found.
  */
 (function($) {
     var options = {
@@ -19,10 +14,13 @@
     };
 
     function reflectDataY(data) {
-        var i;
+        var i,
+            loopTo = data.length;
         // TODO: Flot docs say to look at datapoints.pointsize, instead of
         // assuming 2, but this works for the plot formats we use.
-        for (i = data.length - 2; i >= 0; i -= 2) {
+        data.push(null);
+        data.push(null);
+        for (i = 0; i < loopTo; i +=2 ) {
             data.push(data[i]);
             data.push(data[i + 1] * -1.0);
         }
